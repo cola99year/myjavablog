2e62936b3684186114d5adf0ca85e54b4c29ac79
cola99year <1620921710@qq.com>
2022-06-26 15:00:29 +0800

    项目初始化

---
+++ src/main/java/com/cola/colablog/ColablogApplication.java
@@ -0,0 +1,13 @@
+package com.cola.colablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColablogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColablogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cola/colablog/ColablogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cola.colablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColablogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
